b5f47606fab51d9543d4d04c96657736056cd5c6
penguin-yeh <77048324+penguin-yeh@users.noreply.github.com>
2021-10-03 00:54:21 +0800

    Add files via upload

---
+++ 20200526/1316_mine.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class pairs
+{
+    public:
+        int price;
+        int day;
+};
+
+bool cmp(pairs a, pairs b)
+{
+    return a.price > b.price;
+}
+
+int find(int *days, int day)
+{
+    if(days[day]==-1)
+        return day;
+    else
+        return find(days, days[day]);
+}
+
+int main()
+{
+    int num;
+    int first = 1;
+    while(cin >> num)
+    {
+        pairs object[10240];
+        int days[10240];
+        int cnt = 0;
+        int sum = 0;
+        int tmp;
+        memset(days,-1,sizeof(days));
+        for(int i=0;i<num;i++)
+        {
+            cin >> object[i].price >> object[i].day;
+        }
+        sort(object,object+num,cmp);
+        for(int i=0;i<num;i++)
+        {
+            for(int j=object[i].day;j>=1;j--)
+            {
+                if(days[j]==-1)
+                {
+                    days[j]=1;
+                    sum += object[i].price;
+                    break;
+                }
+            }
+            // tmp = find(days, object[i].day);
+            // if(tmp)
+            // {
+            //     days[tmp] = tmp - 1;
+            //     sum += object[i].price;
+            // }
+
+        }
+        // if(first)
+        //     first = 0;
+        // else
+        //     cout << endl;
+
+        cout << sum << endl ;
+    }
+}
